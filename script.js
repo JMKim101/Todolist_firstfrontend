90db7c8a0fbf8ae92b612a7a3ae0005a92199052
JMKim <165365632+JMKim101@users.noreply.github.com>
2024-09-18 16:19:10 +0900

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+function keyCodeCheck () {
+	console.log(window.event)
+
+    if(window.event.keyCode == 13) {
+        const newLi = document.createElement('li'); 
+      const newBtn = document.createElement('button'); 
+      const newSpan = document.createElement('span');
+    }
+}
