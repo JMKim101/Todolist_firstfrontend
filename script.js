a1980cffb31b60face8b6b5453413253500fb7cb
JaeMin Kim <jeffthehero038@gmail.com>
2024-09-18 17:53:59 +0900

    .

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-function keyCodeCheck () {
-	console.log(window.event)
+const addBtn = document.querySelector('#addBtn'); // 추가 버튼 선택
 
-    if(window.event.keyCode == 13) {
-        const newLi = document.createElement('li'); 
-      const newBtn = document.createElement('button'); 
-      const newSpan = document.createElement('span');
+function keyCodeCheck () { // 엔터키로 추가
+	if(window.event.keyCode === 13 && todoInput.value !== ''){
+        createTodo();
     }
+}
+
+addBtn.addEventListener('click', () => { // + 버튼으로 추가
+    if(todoInput.value !== ''){ // 빈 값 입력 방지
+        createTodo();
+    }
+})
+
+function createTodo() {
+  const todoList = document.querySelector('#todoList');
+  const newLi = document.createElement('li'); 
+  const newBtn = document.createElement('button'); 
+  const newSpan = document.createElement('span');
+  const todoInput = document.querySelector('#todoInput');
+
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+
+  newSpan.textContent = todoInput.value;
+
+  todoList.appendChild(newLi);
+
+  todoInput.value = '';
 }
